a6fb287206a832014ba8f233a60883709e8ae94c
nrulerina <128160230+nrulerina@users.noreply.github.com>
2024-06-18 00:21:08 +0800

    Add files via upload

---
+++ Erina/User.java
@@ -0,0 +1,74 @@
+
+public class User {
+    protected String name;
+    protected String email;
+    protected String address;
+    protected String phoneNumber;
+    protected String memberID;
+    protected Date dateOfBirth;
+    protected String username;
+    protected String password;
+
+    public User(String name, String email, String address, String phoneNumber, String memberID, String username, String password, Date dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.memberID = memberID;
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public boolean login(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
